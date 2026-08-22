f36731a804d46609b33c7caa757c4c1bb81c261a
Hussain13620 <hussainks2087@gmail.com>
2026-08-22 11:02:49 +0530

    Created backend table and essential functions

---
+++ BACKEND/sqlfunc.py
@@ -0,0 +1,52 @@
+import mysql.connector
+def get_db_connection(_host,_user,_password,_database):
+    connection=mysql.connector.connect(host=_host,user=_user,password=_password,database=_database)
+    return connection 
+def createtable(_host,_user,_password,_database,table_name:str,table_structure:dict):
+    connection=get_db_connection(_host,_user,_password,_database)
+    cursor=connection.cursor()
+    column_def=[]
+    for col_name,data_type in table_structure.items():
+        column_def.append(f"{col_name} {data_type}")
+        columns_str=','.join(column_def)
+        query=f"CREATE TABLE IF NOT EXISTS {table_name} ({columns_str}) "
+        cursor.execute(query)
+    cursor.close()
+    connection.close()
+    print(f"The table {table_name} has been created in database {_database}")
+def insertdata(_host,_user,_password,_database,table_name:str,data:dict):
+    connection=get_db_connection(_host,_user,_password,_database)
+    cursor=connection.cursor()
+    columns=",".join(data.keys())
+    values=",".join(data.values())
+    place_holders=",".join(["%s"]*len(data))
+    query=f"INSERT INTO {table_name} ({columns}) VALUES ({place_holders})"
+    cursor.execute(query,values)
+    connection.commit()
+    print(f"Data has been inserted into the table {table_name}")
+    cursor.close()
+    connection.close()
+def displayall(_host,_user,_password,_database,table_name:str):
+    connection=get_db_connection(_host,_user,_password,_database,table_name)
+    cursor=connection.cursor()
+    query=f"SELECT * FROM {table_name}"
+    cursor.execute(query)
+    data=cursor.fetchall()
+    for row in data:
+        print(row)
+    cursor.close()
+    connection.close()
+def createdict():
+    dictn={}
+    no_of_elements=int(input("Enter the no. of elements you want in a dictionary: "))
+    for i in range(0,no_of_elements):
+        key=input("Enter key:")
+        valchoice=int(input("Which data type of value do you want to enter? \n1:string\n2:integer\n3:float:"))
+        value=input("Enter value:")
+        if valchoice==2:
+            value=int(value)
+        if valchoice==3:
+            value=float(value)
+        dictn[key]=value
+        print("element inserted into the dictionary")
+    return dictn
---
+++ BACKEND/tablecreations.py
@@ -0,0 +1,68 @@
+import mysql.connector
+import sqlfunc
+host='localhost'
+user='root'
+password='Hussain13620_root'
+database='Dayflow'
+# 1. User and Authentication
+table_name1='UserAndAuth'
+schema1={"Employee_ID":"INT PRIMARY KEY",
+"Email":"VARCHAR(100) UNIQUE NOT NULL",
+"password_hash":"VARCHAR(255) NOT NULL",
+"UserRole":"VARCHAR(255) DEFAULT 'Employee'",
+"email_ver":"BOOLEAN DEFAULT FALSE"}
+sqlfunc.createtable(host,user,password,database,table_name1,schema1)
+# 2. Employee Profile
+table_name2="EMPprof"
+schema2={"Name":"VARCHAR(100) NOT NULL",
+         "Phone_no":"INT",
+         "Address":"TEXT",
+         "Profile_pic_url":"VARCHAR(255)",
+         "Department":"VARCHAR(50)",
+         "Designation":"VARCHAR(50)",
+         "Manager_id":"INT",
+         "Doc_Links":"TEXT",
+         "DOJ":"DATE"}
+sqlfunc.createtable(host,user,password,database,table_name2,schema2)
+# 3. Attendance Table
+table_name3 = "Attendance"
+schema3 = {
+    "Attendance_ID": "INT AUTO_INCREMENT PRIMARY KEY",
+    "Employee_ID": "INT",
+    "Log_Date": "DATE",
+    "Check_In_Time": "DATETIME",
+    "Check_Out_Time": "DATETIME",
+    "Status": "VARCHAR(20)",
+    "FOREIGN KEY (Employee_ID)": "REFERENCES UserAndAuth(Employee_ID) ON DELETE CASCADE"
+}
+sqlfunc.createtable(host, user, password, database, table_name3, schema3)
+# 4. Leave Requests Table
+table_name4 = "LeaveRequests"
+schema4 = {
+    "Leave_ID": "INT AUTO_INCREMENT PRIMARY KEY",
+    "Employee_ID": "INT",
+    "Leave_Type": "VARCHAR(20)",
+    "Start_Date": "DATE",
+    "End_Date": "DATE",
+    "Remarks": "TEXT",
+    "Status": "VARCHAR(20) DEFAULT 'Pending'",
+    "Admin_Comments": "TEXT",
+    "Approver_ID": "INT",
+    "FOREIGN KEY (Employee_ID)": "REFERENCES UserAndAuth(Employee_ID) ON DELETE CASCADE"
+}
+sqlfunc.createtable(host, user, password, database, table_name4, schema4)
+# 5. Payroll Table
+table_name5 = "Payroll"
+schema5 = {
+    "Payroll_ID": "INT AUTO_INCREMENT PRIMARY KEY",
+    "Employee_ID": "INT",
+    "Base_Salary": "DECIMAL(10, 2)",
+    "Allowances": "DECIMAL(10, 2)",
+    "Deductions": "DECIMAL(10, 2)",
+    "Net_Pay": "DECIMAL(10, 2)",
+    "Pay_Period": "DATE",
+    "FOREIGN KEY (Employee_ID)": "REFERENCES UserAndAuth(Employee_ID) ON DELETE CASCADE"
+}
+sqlfunc.createtable(host, user, password, database, table_name5, schema5)
+
+    
